e8f90a374fa8b64843c882ed9a612b126c85339b
Dom B <domlamont@yahoo.com>
2021-11-08 10:09:54 -0800

    Deleted Functions in Account File

--- account.js
+++ account.js
@@ -3,23 +3,6 @@
 const pin = 1234;
 let balance = 1000;
 
-//Pin Related Functions/////////////
-
-//Returns Pin Number:
-function getPin(){
-    return pin;
-}
-
-//Balance Related Functions/////////
-
-//Returns Account Balance:
-function getBalance(){
-    return balance;
-}
-
-//Receives an Input Number and Adds it to Balance:
-function setBalance(number){
-    balance += number;
-}
-
 //EXPORTS/////////////////
+module.exports.pin = pin;
+module.exports.balance = balance;
