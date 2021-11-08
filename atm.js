f6df771573fe94681d0ffd5f48cfcfbc47b1edf9
Dom B <domlamont@yahoo.com>
2021-11-08 09:25:16 -0800

    Add index, atm and account js files

---
+++ atm.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Function to Get Balance:
+function getBalance(){
+
+}
+
+//Function to Withdraw from Account:
+function withdraw(){
+
+}
+
+//Function to Deposit into Account:
+function deposit(){
+
+}
+
+//Function to Prompt and Validate User for Account Pin:
+function validatePin(){
+    
+}
